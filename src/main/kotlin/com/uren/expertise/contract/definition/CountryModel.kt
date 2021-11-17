94bae7bfbf037a7b69509f8ba758875d07cffc2c
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-18 01:31:09 +0300

    initial message

---
+++ src/main/kotlin/com/uren/expertise/contract/definition/CountryModel.kt
@@ -0,0 +1,12 @@
+package com.uren.expertise.contract.definition
+
+class CountryModel{
+
+    val id: Long? = null
+    var nameEg: String? = null
+    var twoLetterCode: String? = null
+    var threeLetterCode: String? = null
+    var closed: Boolean? = null
+    var name: String? = null
+    var phoneCode: String? = null
+}
---
+++ src/main/kotlin/com/uren/expertise/contract/definition/DistrictModel.kt
@@ -0,0 +1,9 @@
+package com.uren.expertise.contract.definition
+
+class DistrictModel{
+
+    val id: Long? = null
+    var cityCode: String? = null
+    var districtCode: String? = null
+    var districtName: String? = null
+}
