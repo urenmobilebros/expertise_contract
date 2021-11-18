64455d09f3ca33b92edf702d8aac63a006d4abf6
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-18 13:29:19 +0300

    initial message

---
+++ src/main/kotlin/com/uren/expertise/contract/feedback/CallYouModel.kt
@@ -0,0 +1,19 @@
+package com.uren.expertise.contract.feedback
+
+import java.util.*
+
+
+class CallYouModel{
+
+    val id: Long? = null
+    var email: String? = null
+    var nameSurname: String? = null
+    var phoneNumber: String? = null
+    var createDate: Date? = null
+    var updateDate: Date? = null
+    var callYouTypeId: Int? = null
+    var emailSmsPermission: Boolean? = null
+    var serviceTypeId: Int? = null
+    var completed: Boolean? = null
+    var completedTime: Date? = null
+}
