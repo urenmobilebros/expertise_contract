969f93e7e7dca433c5061511da927d7feffae029
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-21 15:17:13 +0300

    new model added

---
+++ src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseAddRequestModel.kt
@@ -0,0 +1,13 @@
+package com.uren.expertise.contract.feedback
+
+import java.util.*
+
+
+class CallYouAddRequestModel{
+
+    var email: String? = null
+    var nameSurname: String? = null
+    var phoneNumber: String? = null
+    var callYouTypeId: Int? = null
+    var emailSmsPermission: Boolean? = null
+}
