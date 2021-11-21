84e9b192754bf16325239060d48d52d4e40f1687
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-21 15:58:00 +0300

    new model added

---
+++ src/main/kotlin/com/uren/expertise/contract/service/ServiceImageAddMultiRequestModel.kt
@@ -0,0 +1,6 @@
+package com.uren.expertise.contract.service
+
+class ServiceImageAddRequestModel{
+    var serviceId: Int? = null
+    var urlList: List<String>? = null
+}
---
+++ src/main/kotlin/com/uren/expertise/contract/service/ServiceImageAddSingleRequestModel.kt
@@ -0,0 +1,6 @@
+package com.uren.expertise.contract.service
+
+class ServiceImageAddMultiRequestModel{
+    var serviceId: Int? = null
+    var urlList: List<String>? = null
+}
