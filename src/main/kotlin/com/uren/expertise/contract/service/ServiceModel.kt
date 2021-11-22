3d771d425d580412fc95e85ed942678c2f581eb2
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-22 15:35:42 +0300

    new model added

---
+++ src/main/kotlin/com/uren/expertise/contract/service/ServiceModel.kt
@@ -0,0 +1,12 @@
+package com.uren.expertise.contract.service
+
+import java.util.*
+
+class ServiceTypeModel{
+
+    val id: Long? = null
+    var name: String? = null
+    var createDate: Date? = null
+    var updateDate: Date? = null
+    var imageUrl: String? = null
+}
