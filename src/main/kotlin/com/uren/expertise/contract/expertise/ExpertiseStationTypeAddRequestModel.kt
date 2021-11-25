a08310e81db7884a76d97612843f7142c6011a12
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-25 14:17:16 +0300

    enums added

--- src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseStationTypeAddRequestModel.kt
+++ src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseStationTypeAddRequestModel.kt
@@ -1,17 +1,5 @@
 package com.uren.expertise.contract.expertise
 
-class ExpertiseAddRequestModel{
-
+class ExpertiseStationTypeAddRequestModel{
     var name: String? = null
-    var phoneNumber: String? = null
-    var mobilePhoneNumber: String? = null
-    var email: String? = null
-    var longitude: Double? = null
-    var latitude: Double? = null
-    var openHour: Int? = null
-    var closeHour: Int? = null
-    var country: String? = null
-    var city: String? = null
-    var district: String? = null
-    var address: String? = null
 }
--- src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseStationTypeModel.kt
+++ src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseStationTypeModel.kt
@@ -1,5 +1,15 @@
 package com.uren.expertise.contract.expertise
 
-class ExpertiseStationTypeAddRequestModel{
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+class ExpertiseStationTypeModel{
     var name: String? = null
+    var createDate: Date? = null
+    var updateDate: Date? = null
+    var userCreated: Int? = null
+    var userUpdated: Int? = null
+    var active: Boolean? = null
 }
