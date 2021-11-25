070e4e1467c8c7b9ca4ac58ce50e90167f9e37bb
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-25 18:08:55 +0300

    new model added

---
+++ src/main/kotlin/com/uren/expertise/contract/enums/DiscountTypeEnum.kt
@@ -0,0 +1,19 @@
+package com.uren.expertise.contract.enums
+
+enum class CallYouTypeEnum(val typeId: Int) {
+    UNKNOWN(0),
+    CALL_YOU_NEGATIVE_EXPERIENCE(1),
+    CALL_YOU_DEMAND_INFO(2),
+    CALL_YOU_BE_INCLUDED_SYSTEM(3),
+    CALL_YOU_THANKS(4),
+    CALL_YOU_OTHER(4);
+
+    companion object {
+        fun getById(typeId: Int): CallYouTypeEnum? {
+            for (e in values()) {
+                if (e.typeId == typeId) return e
+            }
+            return null
+        }
+    }
+}
