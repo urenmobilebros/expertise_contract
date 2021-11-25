7c58dd44cdd44a0166ba3ac9ec5f9d5c1e3e526c
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-25 17:16:22 +0300

    new model added

---
+++ src/main/kotlin/com/uren/expertise/contract/expertise/ExpertiseStationUpdateRequestModel.kt
@@ -0,0 +1,17 @@
+package com.uren.expertise.contract.expertise
+
+import java.math.BigDecimal
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+class ExpertiseStationAddRequestModel{
+
+    var expertiseId: Int? = null
+    var stationTypeId: Int? = null
+    var description: String? = null
+    var amount: BigDecimal? = null
+    var discount: BigDecimal? = null
+    var discountType: Int? = null
+}
