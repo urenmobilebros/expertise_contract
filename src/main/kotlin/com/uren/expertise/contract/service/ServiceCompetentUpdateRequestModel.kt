750a975447618a8eaa941fee440c97793a5a71b6
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-27 22:31:06 +0300

    changes

---
+++ src/main/kotlin/com/uren/expertise/contract/service/ServiceCompetentUpdateRequestModel.kt
@@ -0,0 +1,16 @@
+package com.uren.expertise.contract.service
+
+import java.util.*
+
+class ServiceCompetentModel{
+
+    var id: Long? = null
+    var email: String? = null
+    var name: String? = null
+    var surname: String? = null
+    var phoneNumber: String? = null
+    var createDate: Date? = Calendar.getInstance().time
+    var updateDate: Date? = null
+    var userId: String? = null
+    var nationalityId: String? = null
+}
