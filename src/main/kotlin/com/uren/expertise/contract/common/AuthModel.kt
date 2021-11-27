1f0530176b24a7bc13f285acfd14f61d8a36406d
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-27 11:46:24 +0300

    enums added

--- src/main/kotlin/com/uren/expertise/contract/common/AuthModel.kt
+++ src/main/kotlin/com/uren/expertise/contract/common/AuthModel.kt
@@ -1,15 +1,9 @@
-package com.uren.expertise.contract.definition
+package com.uren.expertise.contract.common
 
 import javax.persistence.*
 import org.jetbrains.annotations.NotNull
 
-class CityModel{
-
-    var id: Long? = null
-    var name: String? = null
-    var cityCode: String? = null
-    var countryTwoLetterCode: String? = null
-    var countryThreeLetterCode: String? = null
-    var longitude: Double? = null
-    var latitude: Double? = null
+class AuthModel{
+    var userId: Long? = null
+    var remoteIp: String? = null
 }
