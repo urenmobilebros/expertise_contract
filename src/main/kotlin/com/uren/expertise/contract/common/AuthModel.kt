47c01bb1f8f4a33d838fd7187c010675f3064e1d
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-11-27 11:45:51 +0300

    changes

---
+++ src/main/kotlin/com/uren/expertise/contract/common/AuthModel.kt
@@ -0,0 +1,15 @@
+package com.uren.expertise.contract.definition
+
+import javax.persistence.*
+import org.jetbrains.annotations.NotNull
+
+class CityModel{
+
+    var id: Long? = null
+    var name: String? = null
+    var cityCode: String? = null
+    var countryTwoLetterCode: String? = null
+    var countryThreeLetterCode: String? = null
+    var longitude: Double? = null
+    var latitude: Double? = null
+}
