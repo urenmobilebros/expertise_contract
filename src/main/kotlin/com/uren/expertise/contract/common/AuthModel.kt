1b6a119ea7792f830743835e2a6cbac47449eefc
ugur.gogebakan <ugur.gogebakan@etstur.com>
2021-12-06 23:34:10 +0300

    auth model deleted

--- src/main/kotlin/com/uren/expertise/contract/common/AuthModel.kt
+++
@@ -1,9 +0,0 @@
-package com.uren.expertise.contract.common
-
-import javax.persistence.*
-import org.jetbrains.annotations.NotNull
-
-class AuthModel{
-    var userId: Long? = null
-    var remoteIp: String? = null
-}
